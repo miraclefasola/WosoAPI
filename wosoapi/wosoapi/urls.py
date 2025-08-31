e494ecb3d28ba5373d1ddaa7223812aaa550dfa1
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-09-01 00:32:34 +0100

    changed url config for easy navigation

--- wosoapi/wosoapi/urls.py
+++ wosoapi/wosoapi/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("wosoapi/", include("api.urls")),
-    path("wosoapi/", include("accounts.urls")),
+    path("", include("api.urls")),
+    path("", include("accounts.urls")),
 ]

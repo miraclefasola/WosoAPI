b0ee26b42ac7b5f9d4aeb8aab093e04845d18943
Miracle Fasola <Fasolamiracle001@gmail.com>
2025-09-23 12:56:02 +0100

    Fixed admin delete server error by handling exception

--- wosoapi/accounts/admin_task.py
+++ wosoapi/accounts/admin_task.py
@@ -7,4 +7,8 @@ User = get_user_model()
 
 def delete_unverified_users(days=7):
     cutoff = timezone.now() - timedelta(days=days)
-    User.objects.filter(is_active=False, date_joined__lt=cutoff).delete()
+    if User.objects.filter(is_active=False, date_joined__lt=cutoff).exists():
+        User.objects.filter(is_active=False, date_joined__lt=cutoff).delete()
+        return "Unverified users deleted successfully."
+    else:
+        return "No unverified users to delete."
